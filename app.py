7d70659058ac4abbf8ed49e942da7d7b16b5913b
Leonardo Kagohara <leokagohara@gmail.com>
2023-12-03 14:19:50 -0300

    Removed tuple

--- app.py
+++ app.py
@@ -3,28 +3,27 @@ from flask import Flask, request
 
 from creteTransfer import create_transfer
 app = Flask(__name__)
-
 @app.route('/invoice', methods=['POST'])
 def webhook_receiver():
     data = request.json
-    
+    print(data["event"]["log"]["type"])
     if data["event"]["log"]["type"] == "credited":
         print("create transfer")
         amount = data["event"]["log"]["invoice"]["amount"]
-        bank_code="00655522",
-        branch_code="0001",
-        account_number="10000-0",
-        account_type="salary",
-        tax_id="38633192810",
-        name="Domonique Whisenhunt",
-        tags=["tests"]
+        bank_code= "00655522"
+        branch_code= "0001"
+        account_number= "10000-0"
+        account_type= "salary"
+        tax_id= "38633192810"
+        name= "Domonique Whisenhunt"
+        tags= ["tests"]
         #bank_code= "20018183"
         #branch_code= "0001"
         #account_number= "6341320293482496"
         #account_type= "payment"
         #tax_id= "20.018.183/0001-80"
         #name= "Stark Bank S.A."
-        #create_transfer(amount,bank_code, branch_code, account_number, account_type, tax_id, name, tags)
+        create_transfer(amount,bank_code, branch_code, account_number, account_type, tax_id, name, tags)
         print(amount)
         print(bank_code)
         print(branch_code)
@@ -36,7 +35,11 @@ def webhook_receiver():
         print("----")
         print(data)
 
-    return '', 200
+    return 'POST ok', 200
+
+@app.route('/', methods=['GET'])
+def get_receiver():
+    return 'Status ativo', 200
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
