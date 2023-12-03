812a626af78107284b07c09f8ba33611c97df0a1
Leonardo Kagohara <leokagohara@gmail.com>
2023-12-03 10:40:41 -0300

    created endpoint to receive stark webhooks

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook_receiver():
+    data = request.json
+    print(data)
+    return '', 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port)
