fb66b1aaeb1d3a49d9ef1599695ad63253353ed7
Leonardo Kagohara <leokagohara@gmail.com>
2023-12-03 22:28:33 -0300

    Updated app.py

--- app.py
+++ app.py
@@ -1,47 +1,34 @@
 import os
 from flask import Flask, request
-import asyncio
 
 from creteTransfer import create_transfer
-
-async def process_webhook(data):
-        # Simulando processamento do webhook
-    await asyncio.sleep(2)
-    print("Webhook processado:", data)
-
-async def webhook_handler():
-    while True:
-        # Aguarde até que haja algum trabalho na fila
-        await asyncio.sleep(0.1)
-
-#async def process_webhook(data):
-            #if data["event"]["log"]["type"] == "credited":
-            #    print("create transfer")
-            #    amount = data["event"]["log"]["invoice"]["amount"]
-            #    bank_code= "00655522"
-            #    branch_code= "0001"
-            #    account_number= "10000-0"
-            #    account_type= "salary"
-            #    tax_id= "38633192810"
-            #    name= "Domonique Whisenhunt"
-            #    tags= ["tests"]
-            #    #bank_code= "20018183"
-            #    #branch_code= "0001"
-            #    #account_number= "6341320293482496"
-            #    #account_type= "payment"
-            #    #tax_id= "20.018.183/0001-80"
-            #    #name= "Stark Bank S.A."
-            #    create_transfer(amount,bank_code, branch_code, account_number, account_type, tax_id, name, tags)    
-
 
 app = Flask(__name__)
 @app.route('/invoice', methods=['POST'])
 def webhook_receiver():
     data = request.json
-#    if data["event"]==false:
-#        return 'error', 400
-#    print(data["event"]["log"]["type"])
-    asyncio.create_task(process_webhook(data))    
+    if data["event"]==false:
+        return 'error', 400
+    print(data["event"]["log"]["type"])
+
+    if data["event"]["log"]["type"] == "credited":
+        print("create transfer")
+        amount = data["event"]["log"]["invoice"]["amount"]
+        bank_code= "00655522"
+        branch_code= "0001"
+        account_number= "10000-0"
+        account_type= "salary"
+        tax_id= "38633192810"
+        name= "Domonique Whisenhunt"
+        tags= ["tests"]
+        #bank_code= "20018183"
+        #branch_code= "0001"
+        #account_number= "6341320293482496"
+        #account_type= "payment"
+        #tax_id= "20.018.183/0001-80"
+        #name= "Stark Bank S.A."
+        create_transfer(amount,bank_code, branch_code, account_number, account_type, tax_id, name, tags)    
+
     return 'POST ok', 200
 
 @app.route('/', methods=['GET'])
--- invoiceIssuer.py
+++ invoiceIssuer.py
@@ -8,8 +8,8 @@ from createPerson import create_person
 from createInvoice import create_invoice
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-i=0
 def issue_invoices():
+    print("rodou as "+ str(datetime.datetime.now()))        
     invoices= random.randint(8, 12)
     payers= create_person(invoices)
 
@@ -18,22 +18,12 @@ def issue_invoices():
         name=payer['name']
         tax_id=payer['tax_id'] 
         tags=["test"]
-        create_invoice(amount=amount, name=name, tax_id=tax_id, tags=tags)
-
-def testar():
-    print("rodou as "+ str(datetime.datetime.now()))
-    print(i)
-          
+        create_invoice(amount=amount, name=name, tax_id=tax_id, tags=tags)  
 
 sched = BlockingScheduler()
-testar()
-@sched.scheduled_job('interval', minutes=1)
+issue_invoices()
+@sched.scheduled_job('interval', hours=3)
 def timed_job():
-    i=+1
-    testar()
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    issue_invoices()
 
 sched.start()
